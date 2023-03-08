222cbf987474d289ba45ee946763b8918db5a414
Matthew Blake <mdblake1872@gmail.com>
2023-03-07 19:22:19 -0800

    adjusting the visualization unit testing

--- tests/viz_tests.py
+++ tests/viz_tests.py
@@ -1,12 +1,15 @@
 import unittest
 
 # The below is to import the function that will produce our figures when we write it
-# from ..src.delays_visualizations import 
+# from ..src.delays_visualizations import fig_function
 
 # This is to perform unit testing on our visualizations 
 
 class UnitTests(unittest.TestCase):
     # This class will be for us to test our visualizations
+	def smoke_test(self):
+		# smoke test to test if our function creates the figure
+		# "plotly" %in% fig_function()
     
 
 if __name__ == "__main__":
