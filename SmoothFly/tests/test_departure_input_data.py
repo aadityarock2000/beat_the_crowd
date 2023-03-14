865bedbac9f1cf017fe0a9835ac037e172d05abe
aadityarock2000 <aadityaparthasarathy@gmail.com>
2023-03-14 03:46:10 -0700

    added test cases for departure_input and modified app.py for that

--- SmoothFly/tests/test_departure_input_data.py
+++ SmoothFly/tests/test_departure_input_data.py
@@ -5,27 +5,43 @@ import pandas as pd
 from departure_input_data import airport_list, carrier_list, file_types
 
 class TestAirportList(unittest.TestCase):
-    def test_airport_list_type(self):
-        self.assertIsInstance(airport_list(), list)
+    def test_smoke(self):
+        # Smoke test
+        path = '../website_utils/reference_data/airport_codes_reference.csv'
+        airports = airport_list(path)
+        self.assertIsInstance(airports, list)
+        self.assertTrue(len(airports) > 0)
+    
+    def test_edge(self):
+        # Edge test
+        path = '../website_utils/reference_data/empty_file.csv'
+        airports = airport_list(path)
+        self.assertEqual(airports, ['All'])
         
     def test_airport_list_contains_all(self):
-        self.assertIn('All', airport_list())
+        self.assertIn('All', airport_list('../website_utils/reference_data/airport_codes_reference.csv'))
         
-    def test_airport_list_contains_reference(self):
-        airport_code_reference=pd.read_csv('../website_utils/reference_data/airport_codes_reference.csv')
-        self.assertCountEqual(list(airport_code_reference['Airport_name']), airport_list()[1:])
         
-# class TestCarrierList(unittest.TestCase):
-#     def test_carrier_list_type(self):
-#         self.assertIsInstance(carrier_list(), list)
-        
-#     def test_carrier_list_contains_all(self):
-#         self.assertIn('All', carrier_list())
-        
-#     def test_carrier_list_contains_reference(self):
-#         airport_carrier_codes_reference=pd.read_csv('website_utils/reference_data/airport_carrier_codes_reference.csv')
-#         self.assertCountEqual(list(airport_carrier_codes_reference['Carrier_name']), carrier_list()[1:])
-        
+class TestCarrierList(unittest.TestCase):
+    def test_smoke_1(self):
+        # Smoke test 1
+        file = '../website_utils/reference_data/airport_carrier_codes_reference.csv'
+        carriers = carrier_list(file)
+        self.assertIsInstance(carriers, list)
+        self.assertTrue(len(carriers) > 0)
+     
+    def test_edge_1(self):
+        # Edge test 1
+        file = '../website_utils/reference_data/empty_file.csv'
+        carriers = carrier_list(file)
+        self.assertEqual(carriers, ['All'])
+    
+    def test_edge_2(self):
+        # Edge test 2
+        file = 'nonexistent_file.csv'
+        with self.assertRaises(FileNotFoundError):
+            carriers = carrier_list(file)
+
 class TestFileTypes(unittest.TestCase):
     def test_file_types_type(self):
         self.assertIsInstance(file_types(), list)
